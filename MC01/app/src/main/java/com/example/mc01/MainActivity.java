e24eb7705bb9f1e4ebca20f7d2010ea7faaf5129
hadiqamazhar <hadiqamazhar49@gmail.com>
2020-09-28 00:22:11 +0500

    Added Counter Text plus java code

--- MC01/app/src/main/java/com/example/mc01/MainActivity.java
+++ MC01/app/src/main/java/com/example/mc01/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.mc01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counte = 0;
+    Button button;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.plainText);
     }
 }
